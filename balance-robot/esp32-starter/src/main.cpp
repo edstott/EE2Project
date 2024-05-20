a3c60b30d5010d1d5c0233ffd984f36859be5881
Ed <ed.stott@imperial.ac.uk>
2024-05-20 17:21:27 +0100

    Add ESP32 starter code

---
+++ balance-robot/esp32-starter/src/main.cpp
@@ -0,0 +1,103 @@
+#include <Arduino.h>
+#include <TimerInterrupt_Generic.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <step.h>
+
+// The Stepper pins
+#define STEPPER1_DIR_PIN 16   //Arduino D9
+#define STEPPER1_STEP_PIN 17  //Arduino D8
+#define STEPPER2_DIR_PIN 4    //Arduino D11
+#define STEPPER2_STEP_PIN 14  //Arduino D10
+#define TOGGLE_PIN  32        //Arduino A4
+
+const int PRINT_INTERVAL = 500;
+const int LOOP_INTERVAL = 10;
+const float kx = 20.0;
+
+#define STEPPER_INTERVAL_US      20
+
+ESP32Timer ITimer(3);
+uint32_t intCount = 0;
+Adafruit_MPU6050 mpu;         //SCL: Arduino D3, SDA: Arduino D4
+
+step step1(STEPPER_INTERVAL_US,STEPPER1_STEP_PIN,STEPPER1_DIR_PIN );
+step step2(STEPPER_INTERVAL_US,STEPPER2_STEP_PIN,STEPPER2_DIR_PIN );
+
+
+//Interrupt Service Routine for motor update
+//Note: ESP32 doesn't support floating point calculations in an ISR
+bool TimerHandler(void * timerNo)
+{
+  static bool toggle = false;
+  step1.runStepper();
+  step2.runStepper();
+  digitalWrite(TOGGLE_PIN,toggle);
+  toggle = !toggle;
+	return true;
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(TOGGLE_PIN,OUTPUT);
+
+  // Try to initialize Accelerometer/Gyroscope
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_44_HZ);
+
+  //Attach motor update ISR to timer to run every STEPPER_INTERVAL_US μs
+  if (!ITimer.attachInterruptInterval(STEPPER_INTERVAL_US, TimerHandler)) {
+    Serial.println("Failed to start stepper interrupt");
+    while (1) delay(10);
+    }
+  Serial.println("Initialised Interrupt for Stepper");
+
+  //Set motor acceleration values
+  step1.setAccelerationRad(10.0);
+  step2.setAccelerationRad(10.0);
+
+}
+
+void loop()
+{
+  //Static variables are initialised once and then the value is remembered betweeen subsequent calls to this function
+  static unsigned long printTimer = 0;  //time of the next print
+  static unsigned long loopTimer = 0;   //time of the next control update
+  static float tiltx = 0.0;             //current tilt angle
+  
+  //Run the control loop every LOOP_INTERVAL ms
+  if (millis() > loopTimer) {
+    loopTimer += LOOP_INTERVAL;
+
+    // Fetch data from MPU6050
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    //Calculate Tilt using accelerometer and sin x = x approximation for a small tilt angle
+    tiltx = a.acceleration.z/9.67;
+
+    //Set target motor speed proportional to tilt angle
+    //Note: this is for demonstrating accelerometer and motors - it won't work as a balance controller
+    step1.setTargetSpeedRad(tiltx*kx);
+    step2.setTargetSpeedRad(-tiltx*kx);
+  }
+  
+  //Print updates every PRINT_INTERVAL ms
+  if (millis() > printTimer) {
+    printTimer += PRINT_INTERVAL;
+    Serial.print(tiltx*1000);
+    Serial.print(' ');
+    Serial.print(step1.getSpeedRad());
+    Serial.println();
+  }
+}
