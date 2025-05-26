f7834a573ab123a6ab225d265d4e07c4a345876e
leo-sawatyanon <leeung.sawatyanon@gmail.com>
2025-05-26 19:41:28 +0100

    Add to vector package

--- maths-accelerator/rayMarcher/VerilogVersion/vector_pkg.sv
+++ maths-accelerator/rayMarcher/VerilogVersion/vector_pkg.sv
@@ -2,6 +2,8 @@ package vector_pkg;
 
   // parameterize your element width
   parameter DATA_WIDTH = 32;
+  parameter FRACT = 16;
+  typedef logic signed [DATA_WIDTH-1:0] num;
 
   // signed fixed-point 3-vector
   typedef struct packed {
@@ -10,6 +12,11 @@ package vector_pkg;
     logic signed [DATA_WIDTH-1:0] z;
   } vec3;
 
+  function automatic vec3 make_vec3(input num x, input num y, input num z);
+    make_vec3.x = x;
+    make_vec3.y = y;
+    make_vec3.z = z;
+  endfunction
   // vector addition
   function automatic vec3 vec3_add(vec3 a, vec3 b);
     vec3_add.x = a.x + b.x;
@@ -31,11 +38,19 @@ package vector_pkg;
     vec3_neg.z = ~a.z + 1;
   endfunction
 
-  // scalar multiply
-  function automatic vec3 vec3_scale(vec3 a, logic signed [DATA_WIDTH-1:0] s);
-    vec3_scale.x = (a.x * s) >>> FRACT; // if fixed-point you shift down by FRACT bits
-    vec3_scale.y = (a.y * s) >>> FRACT;
-    vec3_scale.z = (a.z * s) >>> FRACT;
+  function automatic logic signed [DATA_WIDTH-1:0] vec3_dot(input vec3 a, input vec3 b);
+    logic signed [2*DATA_WIDTH-1:0] xr, yr, zr, sum;
+    xr = $signed(a.x) * $signed(b.x);
+    yr = $signed(a.y) * $signed(b.y);
+    zr = $signed(a.z) * $signed(b.z);
+    vec3_dot = (xr >>> FRACT) + (yr >>>FRACT) + (zr >>> FRACT);
   endfunction
+
+  // scalar multiply
+  // function automatic vec3 vec3_scale(vec3 a, logic signed [DATA_WIDTH-1:0] s);
+  //   vec3_scale.x = (a.x * s) >>> FRACT; // if fixed-point you shift down by FRACT bits
+  //   vec3_scale.y = (a.y * s) >>> FRACT;
+  //   vec3_scale.z = (a.z * s) >>> FRACT;
+  // endfunction
 
 endpackage : vector_pkg
